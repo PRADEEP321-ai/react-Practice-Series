3e1b4b7b7234a1156d9048a7d0c4c1304aed4440
PRADEEP321-ai <156075563+PRADEEP321-ai@users.noreply.github.com>
2026-07-20 11:32:01 +0530

    React js

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    // const addValue = () =>{
+    //   setCount(count + 1);
+    // }
+
+    // const removeValue =  () => {
+    //   setCount(count - 1);
+    // }
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() =>{setCount(count + 1)}}>Increment</button>
+      <button onClick={() => {setCount(count - 1)}}>Decrement</button>
+    </> 
+  )
+}
+
+export default App
